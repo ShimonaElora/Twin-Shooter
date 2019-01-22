f675893f2dadd165a52aa83ac6384030f5658c78
shimona_elora <umysharma@gmail.com>
2019-01-22 06:32:03 +0530

    Input type option add

---
+++ Assets/Scripts/JoystickScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class JoystickScript : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image jsContainer;
+    private Image joystick;
+
+    public Vector3 InputDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jsContainer = GetComponent<Image>();
+        joystick = transform.GetChild(0).GetComponent<Image>(); //this command is used because there is only one child in hierarchy
+        InputDirection = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameMechsScript.isPaused && !GameMechsScript.isGameOver)
+        {
+            if (joystick.rectTransform.anchoredPosition.x > 0)
+            {
+                PlayerScript.shouldMoveRight = true;
+                PlayerScript.shouldMoveLeft = false;
+            }
+            else if (joystick.rectTransform.anchoredPosition.x < 0)
+            {
+                PlayerScript.shouldMoveLeft = true;
+                PlayerScript.shouldMoveRight = false;
+            }
+        }
+    }
+
+    public void OnDrag(PointerEventData ped)
+    {
+        Vector2 position = Vector2.zero;
+
+        //To get InputDirection
+        RectTransformUtility.ScreenPointToLocalPointInRectangle
+                (jsContainer.rectTransform,
+                ped.position,
+                ped.pressEventCamera,
+                out position);
+
+        position.x = (position.x / jsContainer.rectTransform.sizeDelta.x);
+        position.y = (position.y / jsContainer.rectTransform.sizeDelta.y);
+
+        float x = (jsContainer.rectTransform.pivot.x == 1f) ? position.x * 2 : position.x * 2;
+        float y = (jsContainer.rectTransform.pivot.y == 1f) ? position.y * 2 : position.y * 2;
+
+        InputDirection = new Vector3(x, y, 0);
+        InputDirection = (InputDirection.magnitude > 1) ? InputDirection.normalized : InputDirection;
+
+        //to define the area in which joystick can move around
+        if (!GameMechsScript.isPaused && !GameMechsScript.isGameOver)
+        {
+            joystick.rectTransform.anchoredPosition = new Vector3(InputDirection.x * (jsContainer.rectTransform.sizeDelta.x / 3)
+                                                               , InputDirection.y * (jsContainer.rectTransform.sizeDelta.y) / 3);
+        } 
+        else
+        {
+            InputDirection = Vector3.zero;
+            joystick.rectTransform.anchoredPosition = Vector3.zero;
+            PlayerScript.shouldMoveLeft = false;
+            PlayerScript.shouldMoveRight = false;
+        }
+
+    }
+
+    public void OnPointerDown(PointerEventData ped)
+    {
+
+        OnDrag(ped);
+    }
+
+    public void OnPointerUp(PointerEventData ped)
+    {
+
+        InputDirection = Vector3.zero;
+        joystick.rectTransform.anchoredPosition = Vector3.zero;
+    }
+}
--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
 public class PauseScript : MonoBehaviour, IPointerClickHandler
 {
@@ -11,9 +12,30 @@ public class PauseScript : MonoBehaviour, IPointerClickHandler
 
     public GameObject panel;
 
+    public TMP_Dropdown dropDown;
+
+    public GameObject buttons;
+
+    public GameObject joystick;
+
+    //public TextMeshProUGUI textDropDown;
+
     private void Start()
     {
         button = GetComponent<Button>();
+        dropDown.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(dropDown);
+        });
+        if (PlayerPrefs.GetInt("input_type", 0) == 0)
+        {
+            buttons.SetActive(true);
+            joystick.SetActive(false);
+        }
+        else
+        {
+            buttons.SetActive(false);
+            joystick.SetActive(true);
+        }
     }
 
     private void Update()
@@ -32,5 +54,20 @@ public class PauseScript : MonoBehaviour, IPointerClickHandler
     {
         GameMechsScript.isPaused = true;
         panel.SetActive(true);
+        dropDown.value = PlayerPrefs.GetInt("input_type", 0);
+    }
+
+    void DropdownValueChanged(TMP_Dropdown change)
+    {
+        PlayerPrefs.SetInt("input_type", change.value);
+        if (change.value == 0)
+        {
+            buttons.SetActive(true);
+            joystick.SetActive(false);
+        } else
+        {
+            buttons.SetActive(false);
+            joystick.SetActive(true);
+        }
     }
 }
